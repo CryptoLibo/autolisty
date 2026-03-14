ab621f8612d39a26d45aeeab4cc1259faf224460
CryptoLibo <libardobarrios05@gmail.com>
2026-03-14 17:36:58 +0000

    Improve delivery PDF link compatibility

--- lib/r2/generateDeliveryPdf.ts
+++ lib/r2/generateDeliveryPdf.ts
@@ -1,61 +1,65 @@
-import { PDFDocument } from "pdf-lib"
+import { PDFArray, PDFDocument, PDFName, PDFNumber, PDFString } from "pdf-lib"
 import fs from "fs"
 import path from "path"
 
+function createLinkAnnotation(
+  pdfDoc: PDFDocument,
+  rect: [number, number, number, number],
+  url: string
+) {
+  return pdfDoc.context.obj({
+    Type: PDFName.of("Annot"),
+    Subtype: PDFName.of("Link"),
+    Rect: PDFArray.withContext(pdfDoc.context),
+    Border: pdfDoc.context.obj([0, 0, 0]),
+    F: PDFNumber.of(4),
+    H: PDFName.of("I"),
+    A: pdfDoc.context.obj({
+      Type: PDFName.of("Action"),
+      S: PDFName.of("URI"),
+      URI: PDFString.of(url),
+    }),
+  })
+}
+
+function setAnnotationRect(annotation: any, rect: [number, number, number, number], pdfDoc: PDFDocument) {
+  const rectArray = PDFArray.withContext(pdfDoc.context)
+
+  for (const value of rect) {
+    rectArray.push(PDFNumber.of(value))
+  }
+
+  annotation.set(PDFName.of("Rect"), rectArray)
+}
+
 export async function generateDeliveryPdf(listingId: string) {
-
   const templatePath = path.join(process.cwd(), "templates", "delivery_template.pdf")
-
   const templateBytes = fs.readFileSync(templatePath)
 
   const pdfDoc = await PDFDocument.load(templateBytes)
-
   const page = pdfDoc.getPages()[0]
 
   const artworkUrl = `https://download.autolisty.com/artwork/${listingId}`
   const instructionsUrl = `https://download.autolisty.com/instructions/${listingId}`
 
-  const artworkLink = pdfDoc.context.obj({
-    Type: "Annot",
-    Subtype: "Link",
-    Rect: [140, 330, 460, 430],
-    Border: [0, 0, 0],
-    A: {
-      Type: "Action",
-      S: "URI",
-      URI: artworkUrl
-    }
-  })
+  const artworkLink = createLinkAnnotation(pdfDoc, [140, 330, 460, 430], artworkUrl)
+  const instructionsLink = createLinkAnnotation(pdfDoc, [140, 135, 460, 240], instructionsUrl)
 
-  const instructionsLink = pdfDoc.context.obj({
-    Type: "Annot",
-    Subtype: "Link",
-    Rect: [140, 135, 460, 240],
-    Border: [0, 0, 0],
-    A: {
-      Type: "Action",
-      S: "URI",
-      URI: instructionsUrl
-    }
-  })
+  setAnnotationRect(artworkLink, [140, 330, 460, 430], pdfDoc)
+  setAnnotationRect(instructionsLink, [140, 135, 460, 240], pdfDoc)
 
   const artworkRef = pdfDoc.context.register(artworkLink)
   const instructionsRef = pdfDoc.context.register(instructionsLink)
 
-  const annots = page.node.Annots()
+  const existingAnnots = page.node.Annots()
 
-  if (annots) {
-    annots.push(artworkRef)
-    annots.push(instructionsRef)
+  if (existingAnnots) {
+    existingAnnots.push(artworkRef)
+    existingAnnots.push(instructionsRef)
   } else {
-    page.node.set(
-      pdfDoc.context.obj({
-        Annots: [artworkRef, instructionsRef]
-      })
-    )
+    const annots = pdfDoc.context.obj([artworkRef, instructionsRef])
+    page.node.set(PDFName.of("Annots"), annots)
   }
 
-  const pdfBytes = await pdfDoc.save()
-
-  return pdfBytes
+  return await pdfDoc.save()
 }
